3cbd22998335736d0502abbbffd1ac8880a18024
Muhammad Nur fikri <mf4853432@gmail.com>
2023-01-12 14:21:10 +0700

    Tugas 3 Javascript
    Pengumpulan Tugas Niomic

---
+++ tugas3.js
@@ -0,0 +1,11 @@
+//function dengan nilai balik / return
+function rubahNilai(y){
+    //menampilkan nilai 
+    console.log(y);
+    // rubah nilai dari variabel lalu mengembalikan (return)
+    return y + 100;
+}
+
+// Kirim nilai kedalam function (500)
+// Tampil nilai sesudah
+console.log(rubahNilai(500));
